ce58a42bb339d570208ba25634ac807847263a53
jalen-cheng <86445739+jalen-cheng@users.noreply.github.com>
2024-11-09 23:53:55 -0500

    font changes

--- ecoswitch/src/app/page.tsx
+++ ecoswitch/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-ls">
       <div className="flex items-center w-full justify-end">
         <button onClick={toggleModal}>
           <Image 
--- ecoswitch/src/components/Modal.tsx
+++ ecoswitch/src/components/Modal.tsx
@@ -13,7 +13,7 @@ const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60">
-      <div className="font-[var(--font-geist-sans), sans-serif] bg-[#e4e2dd] border-2 border-[#292929] rounded-lg p-6 max-w-md w-full shadow-lg relative">
+      <div className="font-ls, bg-[#e4e2dd] border-2 border-[#292929] rounded-lg p-6 max-w-md w-full shadow-lg relative">
         <button 
           onClick={onClose} 
           className="absolute top-2 right-2 text-[#292929] hover:text-gray-700"
