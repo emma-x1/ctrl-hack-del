829180a790aff445a402c016ebeecfe7e0b98a8b
jalen-cheng <86445739+jalen-cheng@users.noreply.github.com>
2024-11-10 04:17:47 -0500

    test for alternative finder api

---
+++ ecoswitch/src/app/api/findAlt/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+// Ensure OpenAI API Key is set correctly
+const apiKey = process.env.OPENAI_API_KEY;
+if (!apiKey) {
+  throw new Error('The OPENAI_API_KEY environment variable is missing.');
+}
+
+const client = new OpenAI({ apiKey });
+
+// Custom instructions for the OpenAI model
+const INSTRUCTIONS = 'You are tasked with finding a more sustainable alternative to the following product: Respond with just the product name and absolutely nothing else at all. Then, give 3 bullet points identifying why it is a better alternative. Print this in and return it in JSON format with each value below the comma: {"altname": <altname>, "bullet1": "<bullet1>", "bullet2": <bullet2>, "bullet3": <bullet3>}'
+
+    // Call OpenAI API to interpret the images
+    const completion = await client.chat.completions.create({
+        model: 'gpt-4',
+        messages: [
+          {
+            role: 'system',
+            content: INSTRUCTIONS,
+          },
+        ],
+        max_tokens: 1000,
+      });
+
+    // Extract the AI's response
+    const rawAnswer = completion.choices?.[0]?.message?.content as string;
+    const answer = JSON.parse(rawAnswer);
+    const alt = answer?.altname || '';
+    const bp1 = answer?.bullet1 || '';
+    const bp2 = answer?.bullet2 || '';
+    const bp3 = answer?.bullet3 || '';
+
+
+
